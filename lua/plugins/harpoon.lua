5805df24040c37ec048e37385c170fb43b69cf0b
maccarif <fernando.maccari@barrons.com>
2024-06-30 23:52:42 +0300

    chore: update conf

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,11 +6,13 @@ return {
 
     vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Mark File" })
     vim.keymap.set("n", "<C-p>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
+    
+    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
 
-    vim.keymap.set("n", "<C-t>", function()
+    vim.keymap.set("n", "<C-1>", function()
       ui.nav_file(1)
     end, { desc = "Harpoon File 1" })
-    vim.keymap.set("n", "<C-s>", function()
+    vim.keymap.set("n", "<C-2>", function()
       ui.nav_file(2)
     end, { desc = "Harpoon File 2" })
     vim.keymap.set("n", "<C-b>", function()
