e07c7fcc0add9b8ec72f3cda08eefce1f57cc700
maccarif <fernando.maccari@barrons.com>
2024-09-17 21:22:37 +0300

    feat: update comment.nvim to work in jsx

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,10 @@
 return {
   'numToStr/Comment.nvim',
+dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
   config = function()
-    require('Comment').setup()
+    require('Comment').setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
 
     -- vim.keymap.set("n", "<C-_>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
     
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,24 @@
 return {
-	"nvim-treesitter/nvim-treesitter", 
-	build = ":TSUpdate",
-	config = function ()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      context_commentstring = {
+        config = {
+          javascript = {
+            __default = '// %s',
+            jsx_element = '{/* %s */}',
+            jsx_fragment = '{/* %s */}',
+            jsx_attribute = '// %s',
+            comment = '// %s',
+          },
+          typescript = { __default = '// %s', __multiline = '/* %s */' },
+        },
+      }
+    })
+  end
 }
