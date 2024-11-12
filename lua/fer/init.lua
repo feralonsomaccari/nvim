1ac2dd7ba4abab723128feadf165115d7c5effa4
maccarif <fernando.maccari@barrons.com>
2024-11-12 10:55:59 +0000

    feat: prevent navigating to no name buffer

--- lua/fer/init.lua
+++ lua/fer/init.lua
@@ -16,6 +16,7 @@ vim.opt.clipboard:append('unnamedplus') -- Enable system clipboard support
 vim.opt.wildignore:append { '*/node_modules/*' }
 vim.o.cmdheight = 0                     -- Hide the command line
 vim.opt.fillchars = "eob: "
+vim.opt.swapfile = false
 
 -- Search conf
 -- vim.o.hlsearch = false
@@ -50,12 +51,6 @@ function ToggleTreeFocus()
     vim.cmd('wincmd h')
   end
 end
-
--- Exit 'paste mode' after pasting
-vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
-})
 
 -- Map Ctrl+Shift+E to call the custom function
 vim.api.nvim_set_keymap('n', '<C-S-e>', ':lua ToggleTreeFocus()<CR>', { noremap = true, silent = true })
@@ -111,8 +106,11 @@ vim.api.nvim_set_keymap('n', '<C-z>', '<Nop>', { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', 'gg', 'gg0', { noremap = true, silent = true })
 
 -- Chabge CTRL + [ and ] as history
-vim.api.nvim_set_keymap('n', '<C-[>', ':bprev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-]>', ':bnext<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-[>', ':bprev<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-]>', ':bnext<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<C-[>', ':lua GoToPrevBuffer()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-]>', ':lua GoToNextBuffer()<CR>', { noremap = true, silent = true })
 
 -- Move line up with Shift+K
 vim.api.nvim_set_keymap('n', 'K', ':m .-2<CR>==', { noremap = true, silent = true })
@@ -132,22 +130,19 @@ vim.api.nvim_set_keymap('x', 'p', '"_dP', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fn', ":let @/ = expand('<cword>')<CR>n", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>fn', 'y/<C-R><C-O>0<CR>', { noremap = true, silent = true })
 
-
-
-
 -- Utility function to get all valid, listed buffers (ignore [No Name] buffers)
-  function get_listed_buffers()
-    local buffers = {}
-    for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
-      if vim.fn.buflisted(buffer) == 1 and vim.api.nvim_buf_get_name(buffer) ~= "" then
-        table.insert(buffers, buffer)
-      end
+function get_listed_buffers()
+  local buffers = {}
+  for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.fn.buflisted(buffer) == 1 and vim.api.nvim_buf_get_name(buffer) ~= "" then
+      table.insert(buffers, buffer)
     end
-    return buffers
   end
+  return buffers
+end
 
 -- Function to move to the next buffer without cycling
- function next_buffer_no_cycle()
+function next_buffer_no_cycle()
   local buffers = get_listed_buffers()
   local current = vim.api.nvim_get_current_buf()
 
@@ -166,7 +161,7 @@ vim.api.nvim_set_keymap('v', '<leader>fn', 'y/<C-R><C-O>0<CR>', { noremap = true
 end
 
 -- Function to move to the previous buffer without cycling
- function prev_buffer_no_cycle()
+function prev_buffer_no_cycle()
   local buffers = get_listed_buffers()
   local current = vim.api.nvim_get_current_buf()
 
@@ -204,7 +199,7 @@ end
 end
 
 - ]]
---[[ 
+--[[
 -- Function to move the LSP buffer next to the current buffer by swapping
  function move_lsp_buffer_next_to_current(new_buf)
   local current_buf = vim.api.nvim_get_current_buf() -- Get the current buffer
@@ -255,6 +250,83 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 - ]]
 
 
--- Map the keys to the custom buffer navigation functions
-vim.api.nvim_set_keymap('n', '<C-]>', '<cmd>lua next_buffer_no_cycle()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-[>', '<cmd>lua prev_buffer_no_cycle()<CR>', { noremap = true, silent = true })
+-- Enable incremental search
+vim.opt.incsearch = true
+
+-- Define a function to handle the cursor position
+local function handle_search_cursor()
+  -- If the search register is empty (no match), keep the cursor where it is
+  if vim.fn.getreg("/") == "" or vim.v.hlsearch == 0 then
+    -- If no match is found, we want to leave the cursor at its current position
+    return
+  end
+end
+
+-- Create an autocommand that listens to search events
+vim.api.nvim_create_autocmd("CmdlineChanged", {
+  pattern = "/",
+  callback = handle_search_cursor,
+})
+
+-- Function to check if the buffer is unnamed
+function IsUnnamedBuffer(bufnr)
+  return vim.fn.bufname(bufnr) == ""
+end
+
+-- Custom function for navigating to the previous buffer, skipping unnamed buffers
+function GoToPrevBuffer()
+  local buffers = vim.fn.getbufinfo({ buflisted = 1 }) -- Only get listed buffers
+  local current_buf = vim.fn.bufnr('%')
+  local prev_buf = nil
+
+  -- Find the current buffer in the list of buffers
+  for i = #buffers, 1, -1 do
+    if buffers[i].bufnr == current_buf then
+      prev_buf = buffers[i - 1]
+      break
+    end
+  end
+
+  -- Skip unnamed buffers
+  while prev_buf and IsUnnamedBuffer(prev_buf.bufnr) do
+    for i = #buffers, 1, -1 do
+      if buffers[i].bufnr == prev_buf.bufnr then
+        prev_buf = buffers[i - 1]
+        break
+      end
+    end
+  end
+
+  if prev_buf then
+    vim.cmd('buffer ' .. prev_buf.bufnr)
+  end
+end
+
+-- Custom function for navigating to the next buffer, skipping unnamed buffers
+function GoToNextBuffer()
+  local buffers = vim.fn.getbufinfo({ buflisted = 1 }) -- Only get listed buffers
+  local current_buf = vim.fn.bufnr('%')
+  local next_buf = nil
+
+  -- Find the current buffer in the list of buffers
+  for i = 1, #buffers do
+    if buffers[i].bufnr == current_buf then
+      next_buf = buffers[i + 1]
+      break
+    end
+  end
+
+  -- Skip unnamed buffers
+  while next_buf and IsUnnamedBuffer(next_buf.bufnr) do
+    for i = 1, #buffers do
+      if buffers[i].bufnr == next_buf.bufnr then
+        next_buf = buffers[i + 1]
+        break
+      end
+    end
+  end
+
+  if next_buf then
+    vim.cmd('buffer ' .. next_buf.bufnr)
+  end
+end
