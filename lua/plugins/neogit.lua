8f6f74f615e3cc94dc984e49b144cdbe491f111d
maccarif <fernando.maccari@barrons.com>
2024-11-17 17:59:28 +0100

    feat: adds neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,18 @@
+return {
+  'NeogitOrg/neogit',
+  requires = 'nvim-lua/plenary.nvim', -- Required dependency
+  config = function()
+    local neogit = require('neogit')
+    neogit.setup({
+      -- Customize Neogit configuration here
+      integrations = {
+        diffview = true, -- Enable integration with Diffview.nvim if installed
+      },
+    })
+
+    -- Optional keybinding for opening Neogit
+    vim.keymap.set('n', '<leader>gs', function()
+      neogit.open() -- Opens the Neogit status window
+    end, { noremap = true, silent = true })
+  end
+}
