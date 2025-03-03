fbf684c7b1f367afb3d459753e01fc21002bb8bb
maccarif <fernando.maccari@barrons.com>
2025-03-03 20:22:02 +0100

    fix: neo-tree buffer name on blur

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ## TODO
 
-1) Update highlight color on neotree, is hard to see
+1) ~~Update highlight color on neotree, is hard to see~~
 2) Fix navigation inside wkdflow plugin
 3) Neo-tree save session
 4) Navigation in cmp using ctrl+j and k
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,28 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    -- Variable to store the main buffer name
+    local main_buffer_name = ""
+
+    -- Function to get the main buffer name
+    local function get_main_buffer_name()
+      -- If we're in Neo-tree, return the stored main buffer name
+      if vim.bo.filetype == "neo-tree" then
+        return main_buffer_name
+      else
+        -- Update the main buffer name when we are in a regular buffer
+        main_buffer_name = vim.fn.expand('%:t')
+        return main_buffer_name
+      end
+    end
+
+    -- Set up lualine configuration
     require('lualine').setup({
       sections = {
         lualine_a = {},            -- Remove all other sections
         lualine_b = { 'branch' },  -- Only show git branch
         lualine_c = {
-          { 'filename', path = 1 } -- Show full file path and name
+          get_main_buffer_name,    -- Custom function to show the main buffer name
         },
         lualine_x = {},            -- No info on the right
         lualine_y = {},            -- No info on the right
@@ -15,7 +31,9 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {},
+        lualine_c = {
+          get_main_buffer_name,    -- Use the same function for inactive sections
+        },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
@@ -23,7 +41,7 @@ return {
       options = {
         theme = {
           normal = {
-            a = { bg = '#000' }, -- Change this to your preferred background color
+            a = { bg = '#000' }, -- Customize background color
             b = { bg = '#000' },
             c = { bg = '#000' }
           }
@@ -35,3 +53,4 @@ return {
     })
   end
 }
+
