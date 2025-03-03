8ae77b4e23c4648d2412928ac29ea3c5dcced69e
maccarif <fernando.maccari@barrons.com>
2025-03-03 20:21:14 +0100

    fix: highlight color contrast in neo-tree

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -16,10 +16,13 @@ return {
     -- Highlight for the current line number (active line)
     vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#808080' })
 
+    ---- Neotree selected item highlight (background)
+    vim.api.nvim_set_hl(0, 'NeoTreeCursorLine', { bg = '#3e4451' })
+
+
     -- -- Override background color
     -- vim.cmd('highlight Normal guibg=#000')        -- Set background color to red (hex code)
     -- vim.cmd('highlight LineNr guibg=#000')        -- Set line number background color to black (hex code)
-    -- vim.cmd('highlight CursorLineNr guibg=#000')  -- Optional: Set current line number background color to black (hex code)
     -- vim.cmd('highlight SignColumn guibg=#000')
   end
 }
