f5721929c8aa4e037574450d2bbb6fb0ea48e7e2
maccarif <fernando.maccari@barrons.com>
2025-03-05 09:14:50 +0100

    chore: update theme colors

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -2,10 +2,10 @@ return {
   'Mofiqul/vscode.nvim',
   config = function()
     require('vscode').setup({
-      style = 'dark',                  -- Choose between two styles: "dark" or "light"
-      transparent = false,             -- Enable/Disable transparency
-      italic_comments = true,          -- Enable italic comments
-      disable_nvimtree_bg = true       -- Disable nvim-tree background color
+      style = 'dark',            -- Choose between two styles: "dark" or "light"
+      transparent = false,       -- Enable/Disable transparency
+      italic_comments = true,    -- Enable italic comments
+      disable_nvimtree_bg = true -- Disable nvim-tree background color
     })
 
     -- Apply the colorscheme
@@ -24,5 +24,13 @@ return {
     -- vim.cmd('highlight Normal guibg=#000')        -- Set background color to red (hex code)
     -- vim.cmd('highlight LineNr guibg=#000')        -- Set line number background color to black (hex code)
     -- vim.cmd('highlight SignColumn guibg=#000')
+
+    vim.api.nvim_set_hl(0, 'NeoTreeGitIgnored', { fg = '#3a3a3a' })
+    vim.api.nvim_set_hl(0, 'Comment', { fg = '#3a3a3a' })
+
+    vim.api.nvim_set_hl(0, 'VertSplit', { fg = '#1E1E1E' }) -- Vertical split separator
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#1E1E1E' }) -- Status line
+    vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#1E1E1E' }) -- Window separator
   end
+
 }
