5653f077615392533a345da08b153df0935c50e5
maccarif <fernando.maccari@barrons.com>
2025-04-07 11:01:11 +0200

    fix: linting config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,44 +1,36 @@
 return {
-	"nvimtools/none-ls.nvim",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"jay-babu/mason-null-ls.nvim",
-	},
-	config = function()
-		local mason_null_ls = require("mason-null-ls")
-		local null_ls = require("null-ls")
-		local null_ls_utils = require("null-ls.utils")
+  "nvimtools/none-ls.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "jay-babu/mason-null-ls.nvim",
+    "nvimtools/none-ls-extras.nvim",  -- Add the extras plugin
+  },
+  config = function()
+    local mason_null_ls = require("mason-null-ls")
+    local null_ls = require("null-ls")
+    local null_ls_utils = require("null-ls.utils")
 
-		mason_null_ls.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				-- "eslint_d", -- js linter
-			},
-		})
+    mason_null_ls.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "eslint_d", -- js linter
+      },
+    })
 
-		local formatting = null_ls.builtins.formatting -- to setup formatters
-		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+    local formatting = null_ls.builtins.formatting
+    -- local diagnostics = null_ls.builtins.diagnostics
+    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-		null_ls.setup({
-			-- add package.json as identifier for root (for typescript monorepos)
-			root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-			-- setup formatters & linters
-			sources = {
-				--  to disable file types use
-				--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-				formatting.prettier.with({
-					extra_filetypes = { "svelte" },
-				}), -- js/ts formatter
-				formatting.isort,
-				-- diagnostics.eslint_d.with({ -- js/ts linter
-				--   condition = function(utils)
-				--     return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
-				--   end,
-				-- }),
-			},
-	})
-	end,
+    null_ls.setup({
+      root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+      sources = {
+        require("none-ls.diagnostics.eslint_d"),  -- Correct eslint_d source
+        formatting.prettier.with({
+          extra_filetypes = { "svelte" },
+        }),  -- js/ts formatter
+        formatting.isort,
+      },
+    })
+  end,
 }
